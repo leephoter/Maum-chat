17f689f45f026a861cde3ab7997c64a8f532cfc0
leephoter <estarg51@gmail.com>
2022-12-29 02:16:35 +0900

    #4 유저 리스트 key 값 수정

--- renderer/components/UserList.tsx
+++ renderer/components/UserList.tsx
@@ -3,10 +3,9 @@ import styles from './UserList.module.scss';
 
 export default function UserList({ users }) {
   const noUsers = !users.length;
-
   const renderUsers = () =>
-    users.map((user, index) => (
-      <div className={styles.userChat} key={user.displayName + index}>
+    users.map((user) => (
+      <div className={styles.userChat} key={user.uid}>
         <Img src={'/images/logo.png'} width={'50px'} alt="" />
         <div className={styles.userChatInfo}>
           <span>{user.displayName}</span>
